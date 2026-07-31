ef7a15737eedd9bd3a03262d650cb6433cf25143
netadsmedia <139056880+netadsmedia@users.noreply.github.com>
2026-07-31 12:49:54 +0200

    fix(auth): hide unconfigured Google login

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,6 +111,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "apps.common.context_processors.authentication_context",
                 "apps.notifications.context_processors.unread_notification_count",
                 "apps.common.context_processors.sidebar_context",
                 "apps.onboarding.context_processors.onboarding_checklist",
